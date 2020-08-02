7cbda16a5a5d88ac2c51d7d6e79deaa1a6460d49
Efren Ospino <efrenospinojr@gmail.com>
2020-08-02 10:03:38 -0500

    fix crypto function

--- api/functions/crypto.js
+++ api/functions/crypto.js
@@ -2,12 +2,12 @@
 const cryptojs = require("crypto-js");
 const config = require('./../../config');
 
-const encrytp = (data) => {
-    return cryptojs.AES.encrypt(req.body.birthdate, config.secretKey).toString();
+const encrypt = (data) => {
+    return cryptojs.AES.encrypt(data, config.secretKey).toString();
 };
 const decrypt = (data) => {
     const bytes  = cryptojs.AES.decrypt(data, config.secretKey);
     return bytes.toString(cryptojs.enc.Utf8);
 };
-module.exports = {encrytp, decrypt};
+module.exports = {encrypt, decrypt};
 
